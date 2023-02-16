083c1c2d625439ffaa5ea811448036d487f42f4b
Piotr Ropot <Ropot.Piotr@gmail.com>
2023-02-16 21:13:53 +0300

    refactor OnlinerTest, OnlinerPage class

---
+++ src/main/java/OnlinerPage.java
@@ -0,0 +1,17 @@
+public class OnlinerPage {
+
+    public static final String URL = "https://www.onliner.by/";
+    public static final String COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
+    public static final String TXT_COPYRIGHT = "© 2001—2023 Onlíner";
+    public static final String TXT_OPEN = "Вход";
+
+    public static final String BTN_OPEN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+    public static final String TITLE = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String BTN_LOGIN = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+
+    public static final String TITLE_EMAIL = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[1]/div/input";
+    public static final String TITLE_PASSWORD = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[1]/div/input";
+
+    public static final String TXT_TITLE_EMAIL = "Укажите ник или e-mail";
+    public static final String TXT_TITLE_PASSWORD = "Укажите пароль";
+}
--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -1,9 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class OnlinerTest {
 
     @Test
-    public void testMethod (){
+    public void testOpenOnliner() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        WebElement COPYRIGHT_Element = driver.findElement(By.xpath(OnlinerPage.COPYRIGHT));
+        Assert.assertEquals(COPYRIGHT_Element.getText(), OnlinerPage.TXT_COPYRIGHT);
+        driver.quit();
+    }
+
+    @Test
+    public void testOpenOnlinerLoginForm() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        WebElement BTN_OPEN_Element = driver.findElement(By.xpath(OnlinerPage.BTN_OPEN));
+        BTN_OPEN_Element.click();
+        WebElement TITLE_Element = driver.findElement(By.xpath(OnlinerPage.TITLE));
+        Assert.assertEquals(TITLE_Element.getText(), OnlinerPage.TXT_OPEN);
+        driver.quit();
+    }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyCredentials() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5));
+        driver.get(OnlinerPage.URL);
+
+        WebElement BTN_OPEN_Element = driver.findElement(By.xpath(OnlinerPage.BTN_OPEN));
+        BTN_OPEN_Element.click();
+
+        WebElement BTN_LOGIN = driver.findElement(By.xpath(OnlinerPage.BTN_LOGIN));
+        BTN_LOGIN.click();
+
+        WebElement TITLE_EMAIL_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_EMAIL));
+        Assert.assertEquals(TITLE_EMAIL_Element.getText(), OnlinerPage.TXT_TITLE_EMAIL);
+        WebElement TITLE_PASSWORD_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_PASSWORD));
+        Assert.assertEquals(TITLE_PASSWORD_Element.getText(), OnlinerPage.TXT_TITLE_PASSWORD);
+        driver.quit();
 
     }
+
 }
